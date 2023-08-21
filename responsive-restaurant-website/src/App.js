dcf64585dcf0aad6a0b28135277f09b32f27d192
Istyaque Ahammed <istyaque2040@cseku.ac.bd>
2023-08-21 13:34:46 +0600

    Home Page

---
+++ responsive-restaurant-website/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+ 
+import { BrowserRouter as Router } from 'react-router-dom';
+import {GlobalStyle} from './globalStyles';
+import Hero from './components/Hero';
+function App() {
+  return (
+    <Router>
+      <GlobalStyle />
+      
+      <Hero />
+    </Router>
+
+  );
+}
+
+export default App;
