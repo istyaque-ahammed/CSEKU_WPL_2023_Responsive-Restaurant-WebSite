0ca1990fd61362084d205dd274736cc3bfc4696d
Istyaque Ahammed <istyaque2040@cseku.ac.bd>
2023-08-30 13:08:46 +0600

    Footer page

--- responsive-restaurant-website/src/App.js
+++ responsive-restaurant-website/src/App.js
@@ -5,6 +5,7 @@ import Hero from './components/Hero';
 import Products from './components/Products';
 import { productData, productDataTwo } from './components/Products/data';
 import Feature from './components/Feature';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -14,8 +15,7 @@ function App() {
       <Products heading='Delicious Pizza For You' data={productData}/>
       <Feature/>
       <Products heading='Sweet Treats For You' data={productDataTwo}/>
-      
-    
+      <Footer/>    
     </Router>
 
   );
